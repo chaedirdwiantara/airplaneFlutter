5c3aa692a9b6ab32172a5a456e49c73fb120faa1
Chaedir Dwiantara <chaedirdwiantara@gmail.com>
2022-09-20 09:39:41 +0700

    initial state

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:airplane/ui/pages/bonus_page.dart';
+import 'package:airplane/ui/pages/get_started_page.dart';
+import 'package:airplane/ui/pages/main_page.dart';
+import 'package:airplane/ui/pages/sign_up_page.dart';
+import 'package:airplane/ui/pages/splash_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => SplashPage(),
+        '/get-started': (context) => GetStartedPage(),
+        '/sign-up': (context) => SignUpPage(),
+        '/bonus-page': (context) => BonusPage(),
+        '/main': (context) => MainPage(),
+      },
+    );
+  }
+}
