d7450988f645c785725a2bf64ef79e5351434c6a
chaedir dwiantara <chaedirdwiantara@gmail.com>
2024-03-06 12:05:15 +0700

    add main bottom page

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,13 +1,133 @@
+import 'package:airplane/shared/theme.dart';
 import 'package:flutter/material.dart';
-import '../../shared/theme.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Widget customBottomNavigation() {
+      return Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          width: double.infinity,
+          height: 60,
+          margin: EdgeInsets.only(
+            bottom: 30,
+            left: defaultMargin,
+            right: defaultMargin,
+          ),
+          decoration: BoxDecoration(
+            color: kWhiteColor,
+            borderRadius: BorderRadius.circular(18),
+          ),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(),
+                Container(
+                  width: 24,
+                  height: 24,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/icon_home.png'),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 30,
+                  height: 2,
+                  decoration: BoxDecoration(
+                    color: kPrimaryColor,
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(),
+                Container(
+                  width: 24,
+                  height: 24,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/icon_booking.png'),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 30,
+                  height: 2,
+                  decoration: BoxDecoration(
+                    color: kTransparentColor,
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(),
+                Container(
+                  width: 24,
+                  height: 24,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/icon_card.png'),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 30,
+                  height: 2,
+                  decoration: BoxDecoration(
+                    color: kTransparentColor,
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(),
+                Container(
+                  width: 24,
+                  height: 24,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/icon_settings.png'),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 30,
+                  height: 2,
+                  decoration: BoxDecoration(
+                    color: kTransparentColor,
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                ),
+              ],
+            )
+          ]),
+        ),
+      );
+    }
+
     return Scaffold(
-      body: Text('main text'),
+      backgroundColor: kBackgroundColor,
+      body: Stack(
+        children: [
+          const Text('Main Page'),
+          customBottomNavigation(),
+        ],
+      ),
     );
   }
 }
