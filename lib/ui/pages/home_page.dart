a80468d502ca4900e7278d7c77edbe2b8e16e7af
chaedir dwiantara <chaedirdwiantara@gmail.com>
2024-03-12 05:43:16 +0700

    add destination card

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:airplane/shared/theme.dart';
+import 'package:airplane/ui/widgets/destination_card.dart';
 import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
@@ -58,68 +59,42 @@ class HomePage extends StatelessWidget {
     Widget popularDestinations() {
       return Container(
         margin: const EdgeInsets.only(top: 30),
-        child: Row(
-          children: [
-            Container(
-              width: 200,
-              height: 323,
-              margin: EdgeInsets.only(
-                left: defaultMargin,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: const [
+              DestinationCard(
+                name: 'Ciliwoong Lake',
+                city: 'Tenggerang',
+                imageUrl: 'assets/image_destination1.png',
+                rating: 3.0,
               ),
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(18),
-                color: kWhiteColor,
+              DestinationCard(
+                name: 'White House',
+                city: 'Spain',
+                imageUrl: 'assets/image_destination2.png',
+                rating: 4.7,
               ),
-              child: Column(
-                children: [
-                  Container(
-                    width: 180,
-                    height: 220,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(18),
-                      image: const DecorationImage(
-                        image: AssetImage('assets/image_destination1.png'),
-                      ),
-                    ),
-                    child: Align(
-                      alignment: Alignment.topRight,
-                      child: Container(
-                        width: 55,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          color: kWhiteColor,
-                          borderRadius: const BorderRadius.only(
-                            bottomLeft: Radius.circular(18),
-                          ),
-                        ),
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                width: 20,
-                                height: 20,
-                                margin: const EdgeInsets.only(right: 2),
-                                decoration: const BoxDecoration(
-                                  image: DecorationImage(
-                                    image: AssetImage('assets/icon_star.png'),
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                '4.8',
-                                style: blackTextStyle.copyWith(
-                                    fontWeight: medium, fontSize: 14),
-                              )
-                            ]),
-                      ),
-                    ),
-                  )
-                ],
+              DestinationCard(
+                name: 'Hill Yo',
+                city: 'Monaco',
+                imageUrl: 'assets/image_destination3.png',
+                rating: 4.8,
               ),
-            )
-          ],
+              DestinationCard(
+                name: 'Kebon Gede',
+                city: 'Japan',
+                imageUrl: 'assets/image_destination4.png',
+                rating: 5.0,
+              ),
+              DestinationCard(
+                name: 'Patung Aneh',
+                city: 'Singapore',
+                imageUrl: 'assets/image_destination5.png',
+                rating: 2.6,
+              ),
+            ],
+          ),
         ),
       );
     }
