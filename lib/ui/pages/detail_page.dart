26c1152f2c4e26b6eaa442a9f2b639b49b8f26c7
chaedir dwiantara <chaedirdwiantara@gmail.com>
2024-03-13 05:57:19 +0700

    add title and rating

--- lib/ui/pages/detail_page.dart
+++ lib/ui/pages/detail_page.dart
@@ -36,12 +36,84 @@ class DetailPage extends StatelessWidget {
       );
     }
 
+    Widget content() {
+      return Container(
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+        child: Column(children: [
+          // NOTE: EMBLLEM
+          Container(
+            width: 108,
+            height: 24,
+            margin: const EdgeInsets.only(top: 30),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/icon_emblem.png',
+                ),
+              ),
+            ),
+          ),
+
+          // NOTE: TITLE
+          Container(
+            margin: const EdgeInsets.only(top: 256),
+            child: Row(children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Lake Ciliwung',
+                      style: whiteTextStyle.copyWith(
+                        fontSize: 24,
+                        fontWeight: semiBold,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      'Tangerang',
+                      style: whiteTextStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: light,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 20,
+                      height: 20,
+                      margin: const EdgeInsets.only(right: 2),
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/icon_star.png'),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '4.5',
+                      style: whiteTextStyle.copyWith(
+                          fontWeight: medium, fontSize: 14),
+                    )
+                  ]),
+            ]),
+          )
+        ]),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: Stack(
         children: [
           backgroundImage(),
           customShadow(),
+          content(),
         ],
       ),
     );
