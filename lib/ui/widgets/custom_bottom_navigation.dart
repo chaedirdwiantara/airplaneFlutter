cb0d9c9c944133707314c8761ab6e755cbfc528b
chaedir dwiantara <chaedirdwiantara@gmail.com>
2024-04-26 20:09:17 +0700

    add cubit state for the bottom nav

--- lib/ui/widgets/custom_bottom_navigation.dart
+++ lib/ui/widgets/custom_bottom_navigation.dart
@@ -1,38 +1,48 @@
+import 'package:airplane/cubit/page_cubit.dart';
 import 'package:airplane/shared/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CustomBottomNavigationItem extends StatelessWidget {
-  const CustomBottomNavigationItem(
-      {Key? key, required this.imageUrl, this.isSelected = false})
-      : super(key: key);
+  const CustomBottomNavigationItem({
+    Key? key,
+    required this.index,
+    required this.imageUrl,
+  }) : super(key: key);
 
+  final int index;
   final String imageUrl;
-  final bool isSelected;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        const SizedBox(),
-        Container(
-          width: 24,
-          height: 24,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(imageUrl),
-            ),
+    return BlocBuilder<PageCubit, int>(
+      builder: (context, state) {
+        return GestureDetector(
+          onTap: () {
+            context.read<PageCubit>().setPage(index);
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(),
+              Image.asset(
+                imageUrl,
+                width: 24,
+                height: 24,
+                color: state == index ? kPrimaryColor : kGreyColor,
+              ),
+              Container(
+                width: 30,
+                height: 2,
+                decoration: BoxDecoration(
+                  color: state == index ? kPrimaryColor : kTransparentColor,
+                  borderRadius: BorderRadius.circular(18),
+                ),
+              ),
+            ],
           ),
-        ),
-        Container(
-          width: 30,
-          height: 2,
-          decoration: BoxDecoration(
-            color: isSelected ? kPrimaryColor : kTransparentColor,
-            borderRadius: BorderRadius.circular(18),
-          ),
-        ),
-      ],
+        );
+      },
     );
   }
 }
