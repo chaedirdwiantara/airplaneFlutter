b263d42b8ed309aadf2a474a385487edc7aabe18
chaedir dwiantara <chaedirdwiantara@gmail.com>
2024-05-13 14:21:47 +0700

    setup firestore n add auth cubit

---
+++ lib/cubit/auth_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:airplane/models/user_model.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,15 @@ import 'package:airplane/ui/pages/get_started_page.dart';
 import 'package:airplane/ui/pages/main_page.dart';
 import 'package:airplane/ui/pages/sign_up_page.dart';
 import 'package:airplane/ui/pages/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
